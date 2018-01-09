98e3d7276ffc41426d69d2cfa83da398ae99c5c1
Alexis <alexis.poujol@etu.emse.fr>
2018-01-09 16:04:24 +0100

    sart api for building

--- src/main/java/fr/emse/majeureinfo/springbootintro/web/BuildingController.java
+++ src/main/java/fr/emse/majeureinfo/springbootintro/web/BuildingController.java
@@ -1,39 +1,25 @@
-//package fr.emse.majeureinfo.springbootintro.web;
-//
-//import fr.emse.majeureinfo.springbootintro.dao.BuildingDao;
-//import fr.emse.majeureinfo.springbootintro.dao.RoomDao;
-//import fr.emse.majeureinfo.springbootintro.model.Building;
-//import fr.emse.majeureinfo.springbootintro.model.Room;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//import java.util.stream.Collectors;
-//
-//
-//@RestController
-//@RequestMapping(value = "/api/building")
-//@Transactional
-//public class BuildingController {
-//    private final BuildingDao buildingDao;
-//    public BuildingController(BuildingDao buildingDao) { this.buildingDao = buildingDao; }
-//
-//
-//    @GetMapping
-//    public List<BuildingDto> list() {
-//        return buildingDao.findAll().stream().map(BuildingDto::new).collect(Collectors.toList());
-//    }
-//
-//
-//    @GetMapping(value = "/buildingId/{buildingId}")
-//    public BuildingDto get(@PathVariable Long buildingId) {
-//        Building building = buildingDao.getOne(buildingId);
-//        return new BuildingDto(building);
-//    }
-//
-//
-//}
-//
-//
-//
-//
+package fr.emse.majeureinfo.springbootintro.web;
+
+import fr.emse.majeureinfo.springbootintro.dao.BuildingDao;
+import fr.emse.majeureinfo.springbootintro.model.Building;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping(value = "/api/building")
+@Transactional
+public class BuildingController {
+    private final BuildingDao buildingDao;
+    public BuildingController(BuildingDao buildingDao) { this.buildingDao = buildingDao; }
+
+
+
+}
+
+
+
+
